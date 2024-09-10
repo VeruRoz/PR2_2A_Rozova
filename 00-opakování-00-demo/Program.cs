1954b017b6ff09cf7d3445cc7dbbea975d811780
Rozová Veronika <rozove@delta-studenti.cz>
2024-09-10 10:14:48 +0200

    odevzdání demo projektu

---
+++ 00-opakování-00-demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_opakování_00_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
