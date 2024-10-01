ba15ab52055b6689b5b9ea68619ca6db4f7efb16
Rozová Veronika <rozove@delta-studenti.cz>
2024-10-01 09:31:00 +0200

    test

---
+++ 00-Rev-01-poslední-dlouhe/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _00_Rev_01_poslední_dlouhe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] slova = { "Karel", "Eva", "Jaromír", "Iva", "Martin" };
+            Console.WriteLine(PosledniDlouhe(slova, 5));
+            Console.WriteLine(PosledniDlouhe(slova, 7));
+            Console.WriteLine(PosledniDlouhe(slova, 12));
+
+        }
+        public static string PosledniDlouhe(string[] pole, int limit)
+        {
+            string vysledek = "";
+            for(int i = 0; i<pole.Length; i++)
+            {
+                if (pole[i].Length >= limit)
+                    vysledek=pole[i];
+            }
+
+            return vysledek;
+        }
+    }
+}
---
+++ 00-Rev-02-prumer-lichych/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _00_Rev_02_prumer_lichych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[50];
+            while(true)
+            {
+                Console.WriteLine("zadejte celé číslo");
+                string input = Console.ReadLine();
+                int number;
+                if (input == "stop")
+                {
+                    break;
+                }
+                while (!int.TryParse(input, out number))
+                {
+                     Console.WriteLine("zadali jste špatnou hodnotu");
+                     input = Console.ReadLine();
+                }
+                int i = 0;
+                numbers[i] = number;
+                i++;
+            }
+            int početčisel = 0;
+            double prumer = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >0 && numbers[i] %2 !=0)
+                {
+                    početčisel++;
+                    prumer += numbers[i];
+                }
+
+            }
+            prumer = prumer / početčisel;
+            Console.WriteLine(prumer);
+        }
+    }
+}
